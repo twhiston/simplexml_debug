7814219989261a9b8b2a68de0b373ef57eac5f57
Tom Whiston <tom.whiston@gmail.com>
2016-11-27 15:42:41 +0100

    refactor dump, typesafe comparisons

--- src/SxmlDebug.php
+++ src/SxmlDebug.php
@@ -15,7 +15,7 @@ class SxmlDebug {
   const INDENT = "\t";
   const EXTRACT_SIZE = 15;
 
-  private static function AddNamespace($item, &$dump) {
+  private static function AddNamespace(\SimpleXMLElement $item, &$dump) {
 
     // To what namespace does this attribute belong? Returns array( alias => URI )
     $ns = $item->getNamespaces(FALSE);
@@ -32,6 +32,148 @@ class SxmlDebug {
                  PHP_EOL;
       }
     }
+  }
+
+  private static function GetLine($title,
+                                  $data,
+                                  $indent = 1,
+                                  $backtick = TRUE) {
+
+    $indentString = '';
+    while ($indent > 0) {
+      $indentString .= self::INDENT;
+      --$indent;
+    }
+
+    return $indentString . $title . ': ' . ($backtick ? '\'' : '') . $data .
+           ($backtick ? '\'' : '') . PHP_EOL;
+  }
+
+  private static function AddAttribute(\SimpleXMLElement $item, &$dump) {
+
+    $dump .= self::INDENT . 'Attribute {' . PHP_EOL;
+
+    self::AddNamespace($item, $dump);
+
+    $dump .= self::GetLine('Name', $item->getName(), 2);
+    $dump .= self::GetLine('Value', (string) $item, 2);
+
+    $dump .= self::INDENT . '}' . PHP_EOL;
+
+  }
+
+  private static function AddElement(\SimpleXMLElement $item, &$dump) {
+
+    $dump .= self::INDENT . 'Element {' . PHP_EOL;
+
+    self::AddNamespace($item, $dump);
+
+    $dump .= self::GetLine('Name', $item->getName(), 2);
+    $dump .= self::GetLine('String Content', (string) $item, 2);
+
+    // Now some statistics about attributes and children, by namespace
+
+    // This returns all namespaces used by this node and all its descendants,
+    // 	whether declared in this node, in its ancestors, or in its descendants
+    $all_ns = $item->getNamespaces(TRUE);
+    // If the default namespace is never declared, it will never show up using the below code
+    if (!array_key_exists('', $all_ns)) {
+      $all_ns[''] = NULL;
+    }
+
+    foreach ($all_ns as $ns_alias => $ns_uri) {
+      $children = $item->children($ns_uri);
+      $attributes = $item->attributes($ns_uri);
+
+      // Somewhat confusingly, in the case where a parent element is missing the xmlns declaration,
+      //	but a descendant adds it, SimpleXML will look ahead and fill $all_ns[''] incorrectly
+      if (
+        empty($ns_alias)
+        &&
+        NULL !== $ns_uri
+        &&
+        count($children) === 0
+        &&
+        count($attributes) === 0
+      ) {
+        // Try looking for a default namespace without a known URI
+        $ns_uri = NULL;
+        $children = $item->children($ns_uri);
+        $attributes = $item->attributes($ns_uri);
+      }
+
+      // Don't show zero-counts, as they're not that useful
+      if (count($children) === 0 && count($attributes) === 0) {
+        continue;
+      }
+
+      $ns_label = (($ns_alias === '') ? 'Default Namespace' :
+        "Namespace $ns_alias");
+
+      $dump .= self::INDENT . self::INDENT . 'Content in ' . $ns_label .
+               PHP_EOL;
+
+      if (NULL !== $ns_uri) {
+        $dump .= self::GetLine('Namespace URI', $ns_uri, 3);
+      }
+
+
+      $dump .= self::GetLine('Children',
+                             self::GetChildDetails($children),
+                             3,
+                             FALSE);
+
+
+      $dump .= self::GetLine('Attributes',
+                             self::GetAttributeDetails($attributes),
+                             3,
+                             FALSE);
+    }
+
+    $dump .= self::INDENT . '}' . PHP_EOL;
+  }
+
+  private static function GetChildDetails(\SimpleXMLElement $children) {
+    // Count occurrence of child element names, rather than listing them all out
+    $child_names = [];
+    foreach ($children as $sx_child) {
+      // Below is a rather clunky way of saying $child_names[ $sx_child->getName() ]++;
+      // 	which avoids Notices about unset array keys
+      $child_node_name = $sx_child->getName();
+      if (array_key_exists($child_node_name, $child_names)) {
+        $child_names[$child_node_name]++;
+      } else {
+        $child_names[$child_node_name] = 1;
+      }
+    }
+    ksort($child_names);
+    $child_name_output = [];
+    foreach ($child_names as $name => $count) {
+      $child_name_output[] = "$count '$name'";
+    }
+
+    $childrenString = count($children);
+    // Don't output a trailing " - " if there are no children
+    if (count($children) > 0) {
+      $childrenString .= ' - ' . implode(', ', $child_name_output);
+    }
+    return $childrenString;
+  }
+
+  private static function GetAttributeDetails(\SimpleXMLElement $attributes) {
+// Attributes can't be duplicated, but I'm going to put them in alphabetical order
+    $attribute_names = [];
+    foreach ($attributes as $sx_attribute) {
+      $attribute_names[] = "'" . $sx_attribute->getName() . "'";
+    }
+    ksort($attribute_names);
+
+    $attString = count($attributes);
+    // Don't output a trailing " - " if there are no attributes
+    if (count($attributes) > 0) {
+      $attString .= ' - ' . implode(', ', $attribute_names);
+    }
+    return $attString;
   }
 
   /**
@@ -55,6 +197,7 @@ class SxmlDebug {
     // Numeric array indexes, however, operate consistently: $node[0] just returns the node
     $item_index = 0;
     while (isset($sxml[$item_index])) {
+      /** @var \SimpleXMLElement $item */
       $item = $sxml[$item_index];
       $item_index++;
 
@@ -62,121 +205,9 @@ class SxmlDebug {
       // The below relies on the fact that the DOM makes a much clearer distinction
       // Note that this is not an expensive conversion, as we are only swapping PHP wrappers around an existing LibXML resource
       if (dom_import_simplexml($item) instanceOf \DOMAttr) {
-        $dump .= self::INDENT . 'Attribute {' . PHP_EOL;
-
-        self::AddNamespace($item, $dump);
-
-        $dump .= self::INDENT . self::INDENT . 'Name: \'' . $item->getName() .
-                 '\'' .
-                 PHP_EOL;
-        $dump .= self::INDENT . self::INDENT . 'Value: \'' . (string) $item .
-                 '\'' .
-                 PHP_EOL;
-
-        $dump .= self::INDENT . '}' . PHP_EOL;
+        self::AddAttribute($item, $dump);
       } else {
-        $dump .= self::INDENT . 'Element {' . PHP_EOL;
-
-        self::AddNamespace($item, $dump);
-
-        $dump .= self::INDENT . self::INDENT . 'Name: \'' . $item->getName() .
-                 '\'' .
-                 PHP_EOL;
-        // REMEMBER: ALWAYS CAST TO STRING! :)
-        $dump .= self::INDENT . self::INDENT . 'String Content: \'' .
-                 (string) $item .
-                 '\'' . PHP_EOL;
-
-        // Now some statistics about attributes and children, by namespace
-
-        // This returns all namespaces used by this node and all its descendants,
-        // 	whether declared in this node, in its ancestors, or in its descendants
-        $all_ns = $item->getNamespaces(TRUE);
-        // If the default namespace is never declared, it will never show up using the below code
-        if (!array_key_exists('', $all_ns)) {
-          $all_ns[''] = NULL;
-        }
-
-        foreach ($all_ns as $ns_alias => $ns_uri) {
-          $children = $item->children($ns_uri);
-          $attributes = $item->attributes($ns_uri);
-
-          // Somewhat confusingly, in the case where a parent element is missing the xmlns declaration,
-          //	but a descendant adds it, SimpleXML will look ahead and fill $all_ns[''] incorrectly
-          if (
-            $ns_alias == ''
-            &&
-            !is_null($ns_uri)
-            &&
-            count($children) == 0
-            &&
-            count($attributes) == 0
-          ) {
-            // Try looking for a default namespace without a known URI
-            $ns_uri = NULL;
-            $children = $item->children($ns_uri);
-            $attributes = $item->attributes($ns_uri);
-          }
-
-          // Don't show zero-counts, as they're not that useful
-          if (count($children) == 0 && count($attributes) == 0) {
-            continue;
-          }
-
-          $ns_label = (($ns_alias == '') ? 'Default Namespace' :
-            "Namespace $ns_alias");
-          $dump .= self::INDENT . self::INDENT . 'Content in ' . $ns_label .
-                   PHP_EOL;
-
-          if (!is_null($ns_uri)) {
-            $dump .= self::INDENT . self::INDENT . self::INDENT .
-                     'Namespace URI: \'' .
-                     $ns_uri .
-                     '\'' . PHP_EOL;
-          }
-
-          // Count occurrence of child element names, rather than listing them all out
-          $child_names = [];
-          foreach ($children as $sx_child) {
-            // Below is a rather clunky way of saying $child_names[ $sx_child->getName() ]++;
-            // 	which avoids Notices about unset array keys
-            $child_node_name = $sx_child->getName();
-            if (array_key_exists($child_node_name, $child_names)) {
-              $child_names[$child_node_name]++;
-            } else {
-              $child_names[$child_node_name] = 1;
-            }
-          }
-          ksort($child_names);
-          $child_name_output = [];
-          foreach ($child_names as $name => $count) {
-            $child_name_output[] = "$count '$name'";
-          }
-
-          $dump .= self::INDENT . self::INDENT . self::INDENT . 'Children: ' .
-                   count($children);
-          // Don't output a trailing " - " if there are no children
-          if (count($children) > 0) {
-            $dump .= ' - ' . implode(', ', $child_name_output);
-          }
-          $dump .= PHP_EOL;
-
-          // Attributes can't be duplicated, but I'm going to put them in alphabetical order
-          $attribute_names = [];
-          foreach ($attributes as $sx_attribute) {
-            $attribute_names[] = "'" . $sx_attribute->getName() . "'";
-          }
-          ksort($attribute_names);
-          $dump .= self::INDENT . self::INDENT . self::INDENT . 'Attributes: ' .
-                   count($attributes);
-          // Don't output a trailing " - " if there are no attributes
-          if (count($attributes) > 0) {
-            $dump .= ' - ' . implode(', ', $attribute_names);
-          }
-          $dump .= PHP_EOL;
-        }
-
-        $dump .= self::INDENT . '}' . PHP_EOL;
+        self::AddElement($item, $dump);
       }
     }
     $dump .= ']' . PHP_EOL;
@@ -195,7 +226,7 @@ class SxmlDebug {
    * the XML itself. Additionally, the output format is designed as a hint of
    * the syntax needed to traverse the object.
    *
-   * @param \SimpleXMLElement $sxml                   The object to inspect
+   * @param \SimpleXMLElement $sxml The object to inspect
    * @param boolean           $include_string_content Default false. If true,
    *                                                  will summarise textual
    *                                                  content, as well as child
